03440c4d69bd583a39c4cf6b10eed75082e4a66f
fatemanazari <fatemanazari78@gmail.com>
2025-09-21 14:47:17 +0330

    finished all changes

--- script.js
+++ script.js
@@ -1,70 +1,67 @@
 // This function is for the modal of page - more button in the hero section
 
-const  handleModal = () =>{
-  let modal = document.getElementById('modal');
-  if(modal.classList.contains('modal-active')){
-    modal.classList.remove('modal-active')
-  }else{
-    modal.classList.add('modal-active');
+const handleModal = () => {
+  let modal = document.getElementById("modal");
+  if (modal.classList.contains("modal-active")) {
+    modal.classList.remove("modal-active");
+  } else {
+    modal.classList.add("modal-active");
   }
-}
-
+};
 
 // This fuction is handled humberger menu at Moile Version
 const handleMenu = () => document.body.classList.toggle("open_menu");
 const handleBurgerMenu = () => {
+  const cursor = document.getElementById("cursor");
+  const menuItem = document.querySelectorAll(".menu-item");
 
-const cursor = document.getElementById('cursor');
-const menuItem = document.querySelectorAll('.menu-item');
+  document.addEventListener("mousemove", (e) => {
+    cursor.style.left = `${e.clientX}px`;
+    cursor.style.top = `${e.clientY}px`;
+  });
 
-document.addEventListener('mousemove', (e)=>{
-  cursor.style.left = `${e.clientX}px`
-  cursor.style.top = `${e.clientY}px`
-})
+  for (let item of menuItem) {
+    item.addEventListener("mouseenter", () => {
+      cursor.style.opacity = "1";
+      cursor.style.transform = "translate(-50%, -50%) scale(1)";
+    });
 
-for(let item of menuItem){
-  item.addEventListener('mouseenter', ()=>{
-    cursor.style.opacity = '1'
-    cursor.style.transform = 'translate(-50%, -50%) scale(1)'
-  })
-
-  item.addEventListener('mouseleave', ()=>{
-    cursor.style.opacity = '0'
-    cursor.style.transform = 'translate(-50%, -50%) scale(0)'
-  })
-}
-
-}
-
+    item.addEventListener("mouseleave", () => {
+      cursor.style.opacity = "0";
+      cursor.style.transform = "translate(-50%, -50%) scale(0)";
+    });
+  }
+};
 
 // This is for cards section
-const cards = [
-  {
-    title: "Frontend",
-    description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+const services = () => {
+  const cards = [
+    {
+      title: "Frontend",
+      description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, tempora? Est, tempora?`,
-    icon: "./statics/icons/web.svg#web",
-    number: "01"
-  },
-  {
-    title: "Backend",
-    description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      icon: "./statics/icons/web.svg#web",
+      number: "01"
+    },
+    {
+      title: "Backend",
+      description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, tempora? Est, tempora?`,
-    icon: "./statics/icons/web.svg#web",
-    number: "02"
-  },
-  {
-    title: "Full Stack",
-    description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+      icon: "./statics/icons/web.svg#web",
+      number: "02"
+    },
+    {
+      title: "Full Stack",
+      description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit.
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, tempora? Est, tempora?`,
-    icon: "./statics/icons/web.svg#web",
-    number: "03"
-  }
-]
+      icon: "./statics/icons/web.svg#web",
+      number: "03"
+    }
+  ];
 
-let cardHtml = "";
-for(let item of cards){
-  cardHtml += `
+  let cardHtml = "";
+  for (let item of cards) {
+    cardHtml += `
     <div class="services-item">
           <div class="services-item-line"></div>
           <div class="services-item-left">
@@ -80,11 +77,72 @@ for(let item of cards){
             <p>${item.description}</p>
           </div>
         </div>
-  `
-}
+  `;
+  }
+
+  let list = document.getElementById("servicesContainer");
+  list.innerHTML = cardHtml;
+};
+
+services();
+
+//This function is for steps of making mobile application in the page
+
+const steps = () => {
+  const cards = [
+    {
+      icon: "./statics/icons/web.svg#web",
+      number: "01",
+      title: "Expert App Development",
+      paragrapgh:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim quo mollitia fugit accusamus modi. Id culpa magnam distinctio labore?"
+    },
+    {
+      icon: "./statics/icons/web.svg#web",
+      number: "02",
+      title: "Expert App Development",
+      paragrapgh:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim quo mollitia fugit accusamus modi. Id culpa magnam distinctio labore?"
+    },
+    {
+      icon: "./statics/icons/web.svg#web",
+      number: "03",
+      title: "Expert App Development",
+      paragrapgh:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim quo mollitia fugit accusamus modi. Id culpa magnam distinctio labore?"
+    },
+    {
+      icon: "./statics/icons/web.svg#web",
+      number: "04",
+      title: "Expert App Development",
+      paragrapgh:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima enim quo mollitia fugit accusamus modi. Id culpa magnam distinctio labore?"
+    }
+  ];
 
 
-let list = document.getElementById('servicesContainer');
-list.innerHTML = cardHtml;
+  let stepCards = "";
+  for (let items of cards) {
+    stepCards += `
+      <div class="step-cont-item">
+              <div class="step-cont-item-head">
+                <div class="step-cont-item-icon-right">
+                  <svg>
+                     <use href="${items.icon}"></use>
+                  </svg>
+                </div>
+                <div class="step-cont-item-icon-left">${items.number}</div>
+              </div>
+              <div class="step-cont-item-body">
+                <h5>${items.title}</h5>
+                <p>${items.paragrapgh}</p>
+              </div>
+            </div>
+    `;
+  }
 
+  let steps = document.getElementById("steps-container");
+  steps.innerHTML = stepCards;
+};
 
+steps();
